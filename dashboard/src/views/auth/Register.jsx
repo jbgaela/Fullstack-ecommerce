c72e1f84e5245294c564676feec60b93d1eac764
jbgaela <jbgaela@gmail.com>
2025-03-13 15:43:58 +0800

    Added design for register page

--- dashboard/src/views/auth/Register.jsx
+++ dashboard/src/views/auth/Register.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import { FaGoogle } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
 
 const Register = () => {
   return (
@@ -21,6 +23,77 @@ const Register = () => {
                 id="name"
                 required
               />
+            </div>
+
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="text"
+                name="email"
+                placeholder="Email"
+                id="email"
+                required
+              />
+            </div>
+
+            <div className="flex flex-col w-full gap-1 mb-3">
+              <label htmlFor="name">Password</label>
+              <input
+                className="px-3 py-2 outline-none border border-slate-400 bg-transparent rounded-md"
+                type="password"
+                name="password"
+                placeholder="Password"
+                id="password"
+                required
+              />
+            </div>
+
+            <div className="flex items-center w-full gap-3 mb-3">
+              <input
+                className="w-4 h-4 text-blue-600 overflow-hidden bg-gray-200 rounded border-gray-300 focus:ring-blue-500"
+                type="checkbox"
+                name="checkbox"
+                id="checkbox"
+              />
+              <label htmlFor="checkbox">
+                I agree to privacy policy & Terms
+              </label>
+            </div>
+
+            <button className="bg-slate-800 w-full hover:shadow-blue-300/50 hover:shadow-lg text-white rounded-md px-7 py-2 mb-3">
+              Sign Up
+            </button>
+
+            <div className="flex items-center mb-3 gap-3 justify-center">
+              <p>
+                Already have an account?{" "}
+                <Link className="font-bold" to="/login">
+                  Sign in
+                </Link>
+              </p>
+            </div>
+
+            <div className="w-full flex justify-center items-center mb-3">
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+              <div className="w-[10%] flex justify-center items-center">
+                <span className="pb-1">Or</span>
+              </div>
+              <div className="w-[45%] bg-slate-700 h-[1px]"></div>
+            </div>
+
+            <div className="flex justify-center items-center gap-3">
+              <div className="w-[135px] h-[35px] flex rounded-md bg-orange-700 shadow-lg hover:shadow-orange-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span>
+                  <FaGoogle />
+                </span>
+              </div>
+
+              <div className="w-[135px] h-[35px] flex rounded-md bg-blue-700 shadow-lg hover:shadow-blue-700/50 justify-center cursor-pointer items-center overflow-hidden">
+                <span>
+                  <FaFacebook />
+                </span>
+              </div>
             </div>
           </form>
         </div>
