05aa4e839ccf6508d50b783b66ad67142112e8fa
jbgaela <jbgaela@gmail.com>
2025-03-13 21:44:54 +0800

    Added ReduxToolkit and admin login & images

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -2,13 +2,27 @@ import React, { lazy, Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/index";
+import { Toaster } from "react-hot-toast";
 
-const App = lazy(() => import("./App")); // Requires default export in App.js
+const App = lazy(() => import("./App"));
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>
-      <App />
-    </Suspense>
+    <Provider store={store}>
+      <Suspense>
+        <App />
+        <Toaster
+          toastOptions={{
+            position: "top-right",
+            style: {
+              background: "#283046",
+              color: "white",
+            },
+          }}
+        />
+      </Suspense>
+    </Provider>
   </BrowserRouter>
 );
