5b7fb0a8d48a4b6eac7943f7135a65d9db8b349f
jbgaela <jbgaela@gmail.com>
2025-03-14 18:52:07 +0800

    Added database

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const { dbConnect } = require("./utils/db");
 
 app.use(
   cors({
@@ -19,4 +20,5 @@ app.use("/api", require("./routes/authRoutes"));
 
 app.get("/", (req, res) => res.send("My backend"));
 const port = process.env.PORT;
+dbConnect();
 app.listen(port, () => console.log(`Server is running on port ${port}`));
