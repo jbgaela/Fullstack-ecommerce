c367ceec60b3c23d84072dad4d505776b95293b8
jbgaela <jbgaela@gmail.com>
2025-03-14 23:21:08 +0800

    Added token and authentication to admin dashboard

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,12 +1,34 @@
 const adminModel = require("../models/adminModel");
+const { responseReturn } = require("../utils/response");
+const { createToken } = require("../utils/createToken");
+const bcrypt = require("bcrypt");
 
 class authControllers {
   admin_login = async (req, res) => {
     const { email, password } = req.body;
     try {
       const admin = await adminModel.findOne({ email }).select("+password");
-      console.log(admin);
-    } catch (error) {}
+
+      if (admin) {
+        const match = await bcrypt.compare(password, admin.password);
+        if (match) {
+          const token = await createToken({
+            id: admin.id,
+            role: admin.role,
+          });
+          res.cookie("accessToken", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+          });
+          responseReturn(res, 200, { token, message: "Login Success" });
+        } else {
+          responseReturn(res, 404, { error: "Wrong Password" });
+        }
+      } else {
+        responseReturn(res, 404, { error: "Email not found" });
+      }
+    } catch (error) {
+      responseReturn(res, 500, { error: error.message });
+    }
   };
 }
 
--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 module.exports.dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.DB_URL);
-    console.log("Database connected...");
+    await mongoose
+      .connect(process.env.DB_URL)
+      .then(() => console.log("Connected to MongoDB"))
+      .catch((err) => console.error("Error connecting to MongoDB:", err));
   } catch (error) {
     console.log(error.message);
   }
